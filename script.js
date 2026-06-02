1ac35d9d32c2efd499bc84d8282f8bea78e2c9f6
Bianca Olivares <bianca.olivares@ug.uchile.cl>
2026-06-02 17:40:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -174,7 +174,7 @@
                                 backgroundColor: "rgba(54, 162, 235, 0.7)",
                                 borderColor: "rgba(54, 162, 235, 1)",
                                 borderWidth: 1
-                               }
+                               },
 							 ] 
 					},
                         
